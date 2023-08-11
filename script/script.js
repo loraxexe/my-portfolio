67c8169413f726e547ef1797ab5473a95aa4f03d
Ice090 <douglax2005@gmail.com>
2023-08-11 11:44:53 -0300

    feat: links added, menu working, footer fixed
    Co-authored-by: Lúcio Andrade <lucioroadtoglory@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -1,6 +1,16 @@
-const menu = document.querySelector('.menu');
-const toggleMenuButton = document.getElementById('toggleMenu');
+let isVisibleMenu = false;
 
-toggleMenuButton.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
+const buttonMenu = document.getElementById('button-menu');
+
+const toggleMenu = () => {
+  const menu = document.getElementById('menu');
+  if (isVisibleMenu) {
+    menu.classList.remove('visible');
+  } else if (isVisibleMenu === false){
+    menu.classList.add('visible')
+  }
+  isVisibleMenu = !isVisibleMenu 
+}
+
+
+buttonMenu.addEventListener('click', toggleMenu)
