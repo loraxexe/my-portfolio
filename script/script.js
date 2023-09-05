e78bebcec4976cab693ff2c350b49a97d902982e
Ice090 <douglax2005@gmail.com>
2023-09-05 02:13:18 -0300

    remaking

--- script/script.js
+++ script/script.js
@@ -27,40 +27,4 @@ let isVisibleMenu = false;
 
     const code = document.getElementById('card-one')
 
-    code.innerHTML = `
-<div class="card-one">
-<pre>
-<span class="card-cont">.container </span><span class="card-key">&lcub;</span>
-  display: <span class="card-tags">flex;</span>
-  align-items: <span class="card-tags">center;</span>
-  justify-content: <span class="card-tags">center;</span>
-  flex-direction: <span class="card-tags">column;</span>
-<span class="card-key">&rcub;</span></pre>
-</div>
-`
-
-const code2 = document.getElementById('card-two')
-
-code2.innerHTML = `
-<div class="card-item">
-<pre>
-<span class="card-cont">.card </span><span class="card-key">&lcub;</span>
-  width: <span class="card-tags">100%;</span>
-  background: <span class="card-tags">#00ffb3;</span>
-  box-shadow: <span class="card-tags">2px -4px 2px;</span>
-<span class="card-key">&rcub;</span></pre>
-</div>
-`
-
-const code3 = document.getElementById('card-three')
-
-code3.innerHTML = `
-<div class="card-three">
-<pre>
-<span class="card-cont">.menu </span><span class="card-key">&lcub;</span>
-  width: <span class="card-tags">100%;</span>
-  padding: <span class="card-tags">2rem;</span>
-  cursor: <span class="card-tags">pointer;</span>
-<span class="card-key">&rcub;</span></pre>
-</div>
-`
+/**/ 
